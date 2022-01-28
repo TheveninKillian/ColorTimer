2cc8f097288fbe1810417aee5b8f89ca673e3913
Killian <thevenin.killian@gmail.com>
2022-01-28 18:06:04 +0100

    add component Timer

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,14 @@
+<script lang="ts" setup>
+import { useTimeStore } from './stores/time'
+
+const valueTimerDefault = useTimeStore()
+
+const minute = valueTimerDefault.minute
+const second = valueTimerDefault.second
+</script>
+
 <template>
   <main>
-    <Timer />
+    <Timer :minute="minute" :second="second" />
   </main>
 </template>
