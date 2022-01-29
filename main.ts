ddbe0164dce85508d535c427d21b38d4af875e23
Killian <thevenin.killian@gmail.com>
2022-01-29 13:43:11 +0100

    update: add package resize window ration

--- main.ts
+++ main.ts
@@ -1,11 +1,23 @@
 // eslint-disable-next-line @typescript-eslint/no-var-requires
 const { app, BrowserWindow } = require('electron')
+// eslint-disable-next-line @typescript-eslint/no-var-requires
+const AspectRatioBrowserWindow = require('electron-aspect-ratio-browser-window')
 
 function createWindow() {
-  const win = new BrowserWindow({
-    width: 800,
+  const win = new AspectRatioBrowserWindow({
+    width: 960,
     height: 400,
+    backgroundColor: '#000',
+    fullscreenable: false,
+    autoHideMenuBar: true,
+    titleBarStyle: 'hidden',
+    titleBarOverlay: {
+      color: '#000',
+      symbolColor: '#fff',
+    },
   })
+
+  win.setAspectRatio(12 / 5)
 
   win.loadFile('dist/index.html')
 }
