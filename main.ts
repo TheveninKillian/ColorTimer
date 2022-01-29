82a4d9dbfee11aad248b963d06b078cf9c3bdd00
Killian <thevenin.killian@gmail.com>
2022-01-29 15:57:28 +0100

    udpate

--- main.ts
+++ main.ts
@@ -1,4 +1,6 @@
 // eslint-disable-next-line @typescript-eslint/no-var-requires
+// const path = require('path')
+// eslint-disable-next-line @typescript-eslint/no-var-requires
 const { app, BrowserWindow } = require('electron')
 // eslint-disable-next-line @typescript-eslint/no-var-requires
 const AspectRatioBrowserWindow = require('electron-aspect-ratio-browser-window')
@@ -6,18 +8,22 @@ const AspectRatioBrowserWindow = require('electron-aspect-ratio-browser-window')
 function createWindow() {
   const win = new AspectRatioBrowserWindow({
     width: 960,
+    minWidth: 240,
     height: 400,
+    minHeight: 100,
     backgroundColor: '#000',
     fullscreenable: false,
     autoHideMenuBar: true,
-    titleBarStyle: 'hidden',
-    titleBarOverlay: {
-      color: '#000',
-      symbolColor: '#fff',
+    frame: false,
+    webPreferences: {
+      nodeIntegration: true,
+      enableRemoteModule: true,
     },
   })
 
   win.setAspectRatio(12 / 5)
+
+  win.openDevTools()
 
   win.loadFile('dist/index.html')
 }
